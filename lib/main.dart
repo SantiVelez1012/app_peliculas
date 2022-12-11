3ed8eafbe06f43d2bb37f0768c13aa577bb9b19c
Santiago Vélez <santivekez1012@gmail.com>
2022-12-11 14:18:29 -0500

    .

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:app_peliculas/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Peliculas',
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
+      routes: {
+        'home': (__) => const HomeScreen(),
+        'detail': (__) => const DetailScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(child: const Center(child: Text('Detail Screen'))));
+  }
+}
